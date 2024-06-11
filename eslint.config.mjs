cb12a682c84d708e702d4c65a2a5f1a77eff3c5a
kris <krisd3214@gmail.com>
2024-06-11 11:28:33 +0700

    test-lint2

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      semi: ['error', 'always'],
+      quotes: ['error', 'double'],
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      'no-mixed-spaces-and-tabs': ['error', 'smart-tabs']
+    }
+  }
+];
